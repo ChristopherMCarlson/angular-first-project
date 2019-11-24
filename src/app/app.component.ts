1acdebeb0b0d66ab75d0b853955be7d24a5c12fa
Christopher Carlson <c.m.carlson13@gmail.com>
2019-11-23 21:04:15 -0700

    The form works, but its not user friendly yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'angular-first-project';
+  todoArray=[]
+  addTodo(value){
+    this.todoArray.push(value)
+    console.log(this.todoArray)
+  }
+  deleteItem(todo){
+    for(let i=0 ;i<= this.todoArray.length ;i++){
+      if(todo== this.todoArray[i]){
+        this.todoArray.splice(i,1)
+      }
+    }
+  }
+  todoSubmit(value:any){
+    if(value!==""){
+      this.todoArray.push(value.todo)
+    }else{
+        alert('Field required **')
+      }
+    }
 }
